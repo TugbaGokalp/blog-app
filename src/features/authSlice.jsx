37a07535628753fe6602142ff655c8dac11aa25f
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-04-04 22:21:13 +0300

    authSlice was added

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      (state.loading = true), (state.error = false);
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload.user;
+      state.token = null;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.currentUser = null;
+      state.token = null;
+    },
+    registerSuccess: (state, { payload }) => {
+      (state.loading = false), (state.currentUser = payload.user);
+      state.error = false;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+export const {
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+  fetchFail,
+} = authSlice.actions;
+export default authSlice.reducer;
--- src/features/blogSlice.jsx
+++ src/features/blogSlice.jsx
@@ -1,31 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const blogSlice = createSlice({
-  name:"blog" ,
-  initialState:{
-    blogsList:[],
+  name: "blog",
+  initialState: {
+    blogsList: [], //cekilecek veriler
+    categories: [],
     loading: false,
-    error: false
-
+    error: false,
   },
   reducers: {
     fetchstart: (state) => {
-        state.loading = true
-        state.error = false
+      state.loading = true;
+      state.error = false;
     },
-    getSuccess: (state, {payload}) =>{
-        state.loading=false
-        state.blogsList=payload
+    getSuccess: (state, { payload: { data, url } }) => {
+      state.loading = false;
+      state[url] = data;
     },
-    fetchFail:(state) => {
-        state.loading = false
-        state.error = true
-    }
-
-  }
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 });
 
-export const {fetchstart,getSuccess, fetchFail} = blogSlice.actions
+export const { fetchstart, getSuccess, fetchFail } = blogSlice.actions;
 
-export default blogSlice.reducer
+export default blogSlice.reducer;
