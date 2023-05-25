eff87541e76db7755517e04d9d97fc0f9cc5dbb7
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-25 23:06:16 +0300

    footer styling settings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material"
 
 const Footer = () => {
   return (
-    <Box align="center" p={3} component="div" sx={{backgroundColor:"#880e4f", color:"white"}}>
+    <Box align="center" p={2} component="div" sx={{backgroundColor:"#880e4f", color:"white"}}>
         <Typography>Developed by Tugba Gokalp</Typography>
         <Typography>Copyright ©️ Clarusway {new Date().getFullYear()}</Typography>
     </Box>
