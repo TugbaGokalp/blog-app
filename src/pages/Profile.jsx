81594ca8243e756ad212160d0ee78ffb382451d4
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-25 23:14:58 +0300

    profile page was created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
-import { Grid } from "@mui/material"
+import { Grid, ImageListItem, Typography } from "@mui/material"
 
 
 const Profile = () => {
   return (
-    <Grid>
+    <Grid container>
+      <Grid item>
+       <ImageListItem><img src="https://media.istockphoto.com/id/1182641010/tr/foto%C4%9Fraf/evden-%C3%A7al%C4%B1%C5%9Fmay%C4%B1-seviyorum.jpg?s=612x612&w=0&k=20&c=vuHAPC6CkWEke7PbEsL1jqtCH-hp6h2PFRS-JNVsFMs=" alt="blog-photo" /></ImageListItem>
+       <Typography>E-mail: tugbagokalp7265@gmail.com</Typography>
+      </Grid>
 
     </Grid>
   )
