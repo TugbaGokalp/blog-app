60f4773eefde015a33a391cbd8fce888f69f3c29
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-26 17:07:04 +0300

    footer is fixed to page of the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material"
 
 const Footer = () => {
   return (
-    <Box align="center" p={2} component="div" sx={{backgroundColor:"#880e4f", color:"white"}}>
+    <Box  align="center" p={2} component="div" sx={{width: "100%", backgroundColor:"#880e4f", color:"white", position:"fixed", bottom:0}}>
         <Typography>Developed by Tugba Gokalp</Typography>
         <Typography>Copyright ©️ Clarusway {new Date().getFullYear()}</Typography>
     </Box>
