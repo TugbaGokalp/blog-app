226cb04aa7d25e6b6ba4974dce3e2d00063a6896
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-31 11:02:59 +0300

    ToastNotify's added into helper file

---
+++ src/helper/ToastNotify.js
@@ -0,0 +1,35 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const toastWarnNotify = (msg) => {
+  toast.warn(msg, {
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const toastSuccessNotify = (msg) => {
+  toast.success(msg, {
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
