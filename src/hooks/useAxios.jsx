57a2171a2976ed547248dcd2a37378ce4aa4b91a
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-31 11:43:07 +0300

    custom hook (useAxios) for getting data with axios

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+const useAxios = () => {
+    const token = "6ce9839946313a076dc102e2e23b6e7ee5832782"
+    const axiosPublic = axios.create({
+        baseURL: "https://32312.fullstack.clarusway.com/"
+    })
+
+    const axiosWithToken = axios.create({
+        baseURL:"https://32312.fullstack.clarusway.com/",
+        headers:{Authorization: `Token ${token}`}
+    })
+
+    return{axiosPublic, axiosWithToken}
+}
+
+export default useAxios
